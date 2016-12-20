eb9ea4048ab4d69059fec4b28dd836b4b2420f6b
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 12:56:19 -0500

    shoot

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 echo %LIBRARY_PREFIX%
 ls %LIBRARY_PREFIX%
 ls %LIBRARY_PREFIX%/include/
-bash configure --with-netcdf=%LIBRARY_PREFIX% --with-blas=%LIBRARY_PREFIX% --with-bzlib=%LIBRARY_PREFIX% --with-zlib=%LIBRARY_PREFIX% -shared -nomathlib gnu -openblas -noarpack gnu
+bash configure --with-netcdf=%LIBRARY_PREFIX% --with-blas=%LIBRARY_PREFIX% --with-bzlib=%LIBRARY_PREFIX% --with-zlib=%LIBRARY_PREFIX% -shared -openblas -noarpack gnu
 C:/msys64/usr/bin/make libcpptraj
 mkdir -p %PREFIX%/include/cpptraj/
 cp lib/libcpptraj* %PREFIX%/lib/
