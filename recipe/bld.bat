99af9a0639a866a9f6aeeb022e017042e004e5db
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 18:23:08 -0500

    REM

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,21 @@
-::blas?
-:: %PYTHON% -m pip install numpy
-:: %PYTHON% %RECIPE_DIR%\update_configure_win.py
+REM %PYTHON% -m pip install numpy
+REM %PYTHON% %RECIPE_DIR%\update_configure_win.py
 
-:: bash configure2 --with-netcdf=%PREFIX% ^
-::                -nobzlib ^
-::                --with-zlib=%PREFIX% ^
-::                -shared ^
-::                -nomathlib ^
-::                -noreadline ^
-::                -windows ^
-::                gnu
+REM bash configure2 --with-netcdf=%PREFIX% ^
+REM                -nobzlib ^
+REM                --with-zlib=%PREFIX% ^
+REM                -shared ^
+REM                -nomathlib ^
+REM                -noreadline ^
+REM                -windows ^
+REM                gnu
 
 %PYTHON% %RECIPE_DIR%\make_win_config.py
-:: C:\msys64\usr\bin\make libcpptraj
-:: C:\msys64\usr\bin\make
-:: 
-:: mkdir -p %LIBRARY_INC%\cpptraj\
-:: COPY bin\cpptraj.exe %LIBRARY_BIN%\
-:: COPY lib\libcpptraj.so %LIBRARY_LIB%\
-:: XCOPY src\*.h   %LIBRARY_INC%\cpptraj
+
+REM C:\msys64\usr\bin\make libcpptraj
+REM C:\msys64\usr\bin\make
+REM 
+REM mkdir -p %LIBRARY_INC%\cpptraj\
+REM COPY bin\cpptraj.exe %LIBRARY_BIN%\
+REM COPY lib\libcpptraj.so %LIBRARY_LIB%\
+REM XCOPY src\*.h   %LIBRARY_INC%\cpptraj
