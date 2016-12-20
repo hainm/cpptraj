7428c36386464d914d21257558394a58a779f8f7
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 17:33:02 -0500

    make make

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,17 @@
 %PYTHON% %RECIPE_DIR%\update_configure_win.py
 
 bash configure2 --with-netcdf=%PREFIX% ^
-               --with-blas=%PREFIX% ^
-               -openblas ^
                -nobzlib ^
                --with-zlib=%PREFIX% ^
                -shared ^
-               -noarpack ^
+               -nomathlib ^
+               -noreadline ^
                -windows ^
                gnu
 
 :: %PYTHON% %RECIPE_DIR%\make_win_config.py
 C:\msys64\usr\bin\make libcpptraj
+C:\msys64\usr\bin\make
 
 mkdir -p %LIBRARY_INC%\cpptraj\
 COPY bin\cpptraj.exe %LIBRARY_BIN%\
