7f534a548f490d125e428039911756da00be9237
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 17:43:34 -0500

    rm

--- recipe/update_configure_win.py
+++ recipe/update_configure_win.py
@@ -2,4 +2,5 @@ with open('configure') as fh, open('configure2', 'w') as fh2:
     content = fh.read().replace('/', '\\\\')
     content = content.replace('\\\\dev\\\\null', '/dev/null')
     content = content.replace('\\\\dev\\\\stderr', '/dev/stderr')
+    content = content.replace('\\\\bin\\\\rm', '/bin/rm')
     fh2.write(content)
