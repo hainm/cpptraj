4fa659302b35e547e10894ea64786fc29f750970
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 18:12:52 -0500

    ls

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,10 +12,10 @@
 ::                gnu
 
 %PYTHON% %RECIPE_DIR%\make_win_config.py
-C:\msys64\usr\bin\make libcpptraj
-C:\msys64\usr\bin\make
-
-mkdir -p %LIBRARY_INC%\cpptraj\
-COPY bin\cpptraj.exe %LIBRARY_BIN%\
-COPY lib\libcpptraj.so %LIBRARY_LIB%\
-XCOPY src\*.h   %LIBRARY_INC%\cpptraj
+:: C:\msys64\usr\bin\make libcpptraj
+:: C:\msys64\usr\bin\make
+:: 
+:: mkdir -p %LIBRARY_INC%\cpptraj\
+:: COPY bin\cpptraj.exe %LIBRARY_BIN%\
+:: COPY lib\libcpptraj.so %LIBRARY_LIB%\
+:: XCOPY src\*.h   %LIBRARY_INC%\cpptraj
--- recipe/make_win_config.py
+++ recipe/make_win_config.py
@@ -70,4 +70,5 @@ with open('testp.cpp', 'w') as fh:
     int main() { printf("Testing\n"); printf("%s\n",nc_strerror(0)); return 0; }
     """.strip())
 
+subprocess.call('ls {}'.format(include_dir), shell=True)
 subprocess.call('g++ {include_dir} -o testp testp.cpp'.format(include_dir=include_dir), shell=True)
