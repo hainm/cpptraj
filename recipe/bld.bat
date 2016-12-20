7f4a6a344726cf09222613ef3b3c3ef319fa93d5
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 12:27:21 -0500

    fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,4 @@
-bash configure --with-netcdf=%PREFIX% \
-                 --with-blas=%PREFIX% \
-                 --with-bzlib=%PREFIX% \
-                 --with-zlib=%PREFIX% \
-                 -shared \
-                 -nomathlib gnu
-#                -openblas -noarpack gnu
+bash configure --with-netcdf=%PREFIX% --with-blas=%PREFIX% --with-bzlib=%PREFIX% --with-zlib=%PREFIX% -shared -nomathlib gnu -openblas -noarpack gnu
 make libcpptraj -j4
 mkdir -p %PREFIX%/include/cpptraj/
 cp lib/libcpptraj* %PREFIX%/lib/
