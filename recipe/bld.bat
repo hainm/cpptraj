6734fa60a01385efd8ed8abe53890e6c33bbe17b
Hai Nguyen <hainm.comp@gmail.com>
2016-12-20 13:16:12 -0500

    -windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,6 +15,7 @@ bash configure --with-netcdf=%LIBRARY_PREFIX_CW% ^
                --with-zlib=%LIBRARY_PREFIX_CW% ^
                -shared ^
                -noarpack ^
+               -windows ^
                gnu
 C:/msys64/usr/bin/make libcpptraj
 mkdir -p %PREFIX%/include/cpptraj/
