5fd9f297868a99d876213fbbda167f532832e35d
Hai Nguyen <hainm.comp@gmail.com>
2016-12-22 11:42:59 -0500

    try me try me

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,10 +12,10 @@ REM                gnu
 
 %PYTHON% %RECIPE_DIR%\make_win_config.py
 
-REM C:\msys64\usr\bin\make libcpptraj
-REM C:\msys64\usr\bin\make
-REM 
-REM mkdir -p %LIBRARY_INC%\cpptraj\
-REM COPY bin\cpptraj.exe %LIBRARY_BIN%\
-REM COPY lib\libcpptraj.so %LIBRARY_LIB%\
-REM XCOPY src\*.h   %LIBRARY_INC%\cpptraj
+C:\msys64\usr\bin\make libcpptraj
+C:\msys64\usr\bin\make
+
+mkdir -p %LIBRARY_INC%\cpptraj\
+COPY bin\cpptraj.exe %LIBRARY_BIN%\
+COPY lib\libcpptraj.so %LIBRARY_LIB%\
+XCOPY src\*.h   %LIBRARY_INC%\cpptraj
