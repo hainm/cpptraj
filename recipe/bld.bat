d0e0a5c888f9a5391cdff66a8c54a6c66fc217d9
Hai Nguyen <hainm.comp@gmail.com>
2016-12-22 21:18:17 -0500

    f prefix again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,8 @@
-setlocal enableextensions enabledelayedexpansion
-
-@echo off
-
-REM Setting variables in Cygwin style
-set PREFIX_CW=!PREFIX:\=/!
-set PREFIX_CW=!PREFIX_CW::=!
-set PREFIX_CW=/%PREFIX_CW%
-
 %PYTHON% %RECIPE_DIR%/update_configure_win.py
-bash configure2 --with-netcdf=%PREFIX_CW%/Library ^
+echo %PREFIX%
+bash configure2 --with-netcdf=%PREFIX%/Library ^
                -nobzlib ^
-               --with-zlib=%PREFIX_CW%/Library ^
+               --with-zlib=%PREFIX%/Library ^
                -shared ^
                -nomathlib ^
                -noreadline ^
