67a1646f2926cd6135b07912b34f53936fd1e097
Hai Nguyen <hainm.comp@gmail.com>
2016-12-22 21:03:14 -0500

    try cygwin style

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,23 @@
-bash configure --with-netcdf=%PREFIX%\Library ^
+setlocal enableextensions enabledelayedexpansion
+
+@echo off
+
+REM Setting variables in Cygwin style
+set PREFIX_CW=!PREFIX:\=/!
+set PREFIX_CW=!PREFIX_CW::=!
+set PREFIX_CW=/%PREFIX_CW%
+
+%PYTHON% %RECIPE_DIR%/update_configure_win.py
+bash configure2 --with-netcdf=%PREFIX%/Library ^
                -nobzlib ^
-               --with-zlib=%PREFIX%\Library ^
+               --with-zlib=%PREFIX%/Library ^
                -shared ^
                -nomathlib ^
                -noreadline ^
                -windows ^
                gnu
 
-:: %PYTHON% %RECIPE_DIR%\make_win_config.py
+REM %PYTHON% %RECIPE_DIR%\make_win_config.py
 
 C:\msys64\usr\bin\make libcpptraj
 REM C:\msys64\usr\bin\make
